bc9731a037769602d8ba1f1d23caf3da8320b907
Jordy Bollon <jbollon@iit.local@IITCMP3VDAWS010.iit.local>
2023-11-22 12:05:34 +0100

    update

--- sh_files/download_data.sh
+++ sh_files/download_data.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+echo "Downloading Containers.zip"
+mkdir tmp
+wget -O ./tmp/Containers.zip -c https://figshare.com/ndownloader/files/43243176
+cd tmp
+unzip Containers.zip
+rsync -avhz Containers/* ../Containers/
+cd ..
+rm -r tmp
+
 echo "Downloading Data.zip from https://figshare.com/ndownloader/files/42929275"
 wget -O ./Data.zip -c https://figshare.com/ndownloader/files/42929275
 
