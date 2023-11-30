8ee44f3584867d327e203d118b08362c1633769f
Jordy Bollon <57363636+jbollon@users.noreply.github.com>
2023-11-30 10:53:18 +0100

    Update get_latex_table.R

--- utils_function/get_latex_table.R
+++ utils_function/get_latex_table.R
@@ -67,6 +67,39 @@ final_tab = cbind(c(vec_dat, NA), c(vec_prot, NA), final_tab)
 print(xtable(final_tab, type = "latex"), include.rownames=FALSE)
 
 ##################################################################################################
+print("gene_w_more_ISO")
+##################################################################################################
+final_tab = rbind()
+vec_dat = c()
+vec_prot = c()
+
+for (dat in c("jurkat", "wtc11")) {
+  if(dat == "wtc11"){
+    DATA_name = "WTC-11"
+  }else{
+    DATA_name = "jurkat"
+  }
+  proteases = list.dirs(glue("{PATH_WD}/Benchmark_results/{dat}"),
+                        recursive = FALSE,
+                        full.names = FALSE
+  )
+  for (prot in proteases) {
+    vec_dat = c(vec_dat, DATA_name)
+    vec_prot = c(vec_prot, prot)
+    tab = data.table::fread(glue("{PATH_WD}/Benchmark_results/{dat}/{prot}/SumTab_main_result_gene_w_more_ISO.csv"))
+    i_ord = match(c("IsoBayes_mRNA", "IsoBayes", "EPIFANY", "PIA", "Fido"),
+                  tab$Model
+    )
+    final_tab = rbind(final_tab, t(tab[i_ord, "AUC"]))
+  }
+}
+final_tab = rbind(final_tab, colMeans(final_tab))
+final_tab = round(final_tab, 2)
+final_tab = cbind(c(vec_dat, NA), c(vec_prot, NA), final_tab)
+
+print(xtable(final_tab, type = "latex"), include.rownames=FALSE)
+
+##################################################################################################
 print("Correlation openms") 
 ##################################################################################################
 final_tab = rbind()
@@ -74,7 +107,7 @@ vec_method = c("IsoBayes_mRNA", "IsoBayes")
 
 for (mrna in c("_mRNA", "")) {
   row_val = c()
-  for (noUP in c("", "no_UP_")) {
+  for (noUP in c("", "no_UP_", "gene_w_more_ISO_")) {
     for (dat in c("jurkat", "wtc11")) {
       load(glue("{PATH_WD}/Benchmark_results/{dat}/{noUP}scatterplot_benchmark{mrna}.rdata"))
       correlation = strsplit(scat_bench$labels$subtitle, " ")[[1]][2]
@@ -95,7 +128,7 @@ vec_method = c("IsoBayes_mRNA", "IsoBayes")
 
 for (mrna in c("_mRNA", "")) {
   row_val = c()
-  for (noUP in c("", "no_UP_")) {
+  for (noUP in c("", "no_UP_", "gene_w_more_ISO_")) {
     for (dat in c("jurkat", "wtc11")) {
       load(glue("{PATH_WD}/Benchmark_results/{dat}/{noUP}scatterplot_log2fc{mrna}.rdata"))
       correlation = strsplit(scat_bench$labels$subtitle, " ")[[1]][2]
@@ -137,7 +170,7 @@ vec_method = c("IsoBayes_mRNA", "IsoBayes")
 
 for (mrna in c("_mRNA", "")) {
   row_val = c()
-  for (noUP in c("", "no_UP_")) {
+  for (noUP in c("", "no_UP_", "gene_w_more_ISO_")) {
     for (dat in c("jurkat", "wtc11")) {
       load(glue("{PATH_WD}/Benchmark_results/{dat}/{noUP}scatterplot_log2fc{mrna}.rdata"))
       correlation = strsplit(scat_bench$labels$subtitle, " ")[[1]][2]
